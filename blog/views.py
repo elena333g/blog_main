5e74e494569b7e19878824c8d6db36d55e0cf962
elena333g <elenabalbuena333@gmail.com>
2026-04-16 17:35:22 -0700

    Modificacion

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, render,redirect
 
 from .models import Post, Category
-
+from .forms import commentFormm
 # Create your views here.
 def home(request):
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
@@ -14,8 +14,18 @@ def home(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
+    
+
+    if request.method == "POST":
+      form = commentFormm(request.POST)
+
+      if form.is_valid():
+         comment = form.save(commit=False)
+         comment.post = post
+         comment.save()
+
 
     context = {
-        "post": post,
-  }
-    return render(request, "blog/detail",context)
+        "post" : post,
+    }
+    return render(request, "blog/detail.html", context)
